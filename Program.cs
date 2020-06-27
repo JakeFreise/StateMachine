de670bc0cd515e32a293a1832651f59e8a5cd12a
JakeFreise <36875987+JakeFreise@users.noreply.github.com>
2020-06-27 10:56:29 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using StateMachine;
-
+//TEST PROGRAM SHOWING SIMPLE IMPLEMENTATION OF API
 namespace Simulation
 {
     internal class Program
     {
         static void Main()
         {
+            //string location of the System Dynamics Model file
             string xml = "../../Fishmodel.stmx";
 
+            //Model object using location
             Model testModel = new Model(xml);
 
+            //run 30 timesteps and print the number of fish at each time step.
             for (int counter = 0; counter < 30; counter++)
             {
                 Console.WriteLine("Fish = " + testModel.getVariable("Fish"));
